0d296910d3a80374808f46696a4c493cec511882
Daniel Dilts <diltsman@gmail.com>
2025-05-26 00:19:46 -0600

    All exception are are routed into Rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::global_asm, panic::PanicInfo};
+use core::{arch::global_asm, panic::PanicInfo, u32};
 
 global_asm!(include_str!("handlers.S"));
 
@@ -11,6 +11,23 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start() -> () {
     loop {}
 }
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _svc_handler() -> () {}
+
+#[unsafe(no_mangle)]
+#[allow(unused_variables)]
+pub extern "C" fn _abort_prefetch_handler(instruction_addr: u32) -> () {}
+
+#[unsafe(no_mangle)]
+#[allow(unused_variables)]
+pub extern "C" fn _abort_data_handler(instruction_addr: u32) -> () {}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _irq_handler() -> () {}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _fiq_handler() -> () {}
