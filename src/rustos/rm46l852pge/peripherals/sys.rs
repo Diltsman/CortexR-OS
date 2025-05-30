9c3a55c1244ab45a70081f886470f935cb26ec04
Daniel Dilts <diltsman@gmail.com>
2025-05-29 21:12:43 -0600

    Move SYS array into a struct

--- src/rustos/rm46l852pge/peripherals/sys.rs
+++ src/rustos/rm46l852pge/peripherals/sys.rs
@@ -1,12 +1,19 @@
 use core::ptr::{read_volatile, write_volatile};
 
+#[repr(C)]
+struct SysStruct {
+    registers: [u32; 0xfc / 4],
+}
+
 // Init is required by Rust, but isn't used due to linker script NOLOAD
 #[unsafe(link_section = ".sys")]
-static mut SYS: [u32; 0xFC / 4] = [0; 0xfc / 4];
+static mut SYS: SysStruct = SysStruct {
+    registers: [0; 0xfc / 4],
+};
 
 pub fn set_pena() -> () {
     let offset = 0xd0 / 4;
-    let y = unsafe { &raw mut (SYS[offset]) };
+    let y = unsafe { &raw mut (SYS.registers[offset]) };
     let mut x: u32 = unsafe { read_volatile(y) };
     x |= 0x100;
     unsafe { write_volatile(y, x) };
