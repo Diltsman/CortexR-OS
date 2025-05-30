1a67328b8ba3d302f9b8bef550da62968852b0fb
Daniel Dilts <diltsman@gmail.com>
2025-05-29 20:46:17 -0600

    Register offset in const

--- src/rustos/rm46l852pge/peripherals/sys.rs
+++ src/rustos/rm46l852pge/peripherals/sys.rs
@@ -1,12 +1,13 @@
 use core::ptr::{read_volatile, write_volatile};
 
+// Init is required by Rust, but isn't used due to linker script NOLOAD
 #[unsafe(link_section = ".sys")]
 static mut SYS: [u32; 0xFC / 4] = [0; 0xfc / 4];
 
 pub fn set_pena() -> () {
-    let y = unsafe { &raw const (SYS[0xd0 / 4]) };
+    let offset = 0xd0 / 4;
+    let y = unsafe { &raw mut (SYS[offset]) };
     let mut x: u32 = unsafe { read_volatile(y) };
     x |= 0x100;
-    let y = unsafe { &raw mut (SYS[0xd0 / 4]) };
     unsafe { write_volatile(y, x) };
 }
